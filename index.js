52bbde3a85e35d23c0b3b3c721fcaefb528884ff
oinochoe <oinochoe@naver.com>
2020-12-02 10:31:21 +0900

    favicon generator one

---
+++ index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const pngToIco = require('png-to-ico');
+const path = './input';
+
+pngToIco([
+    `${path}/favicon16.png`,
+    `${path}/favicon32.png`,
+    `${path}/favicon48.png`,
+    `${path}/favicon64.png`,
+    `${path}/favicon114.png`,
+    `${path}/favicon128.png`,
+    `${path}/favicon256.png`,
+]).then((buf) => {
+    fs.writeFileSync('./output/favicon.ico', buf);
+})
+.catch(console.error);
